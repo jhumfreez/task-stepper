db0b75d0c3e686b6c30d0a883cb0f1ca140efdb6
jhumfreez <25644264+jhumfreez@users.noreply.github.com>
2023-02-05 14:36:25 -0600

    1 file modified

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, filter, map, Observable } from 'rxjs';
 import { INIT_TASKS } from './constants';
 import { LastLockableTask, Task, Tasks, TaskStatus, TaskType } from './types';
 
@@ -10,19 +10,19 @@ export class TaskService {
   // TODO: Move to state selector
   private _steps: Tasks;
 
-  readonly step$: BehaviorSubject<Tasks>;
+  private _step$: BehaviorSubject<Tasks>;
+
+  private isVisible = (steps: Task) => steps.status !== TaskStatus.Hidden;
 
   get visibleSteps() {
     return this.getState()
-      .filter((steps) => steps.status !== TaskStatus.Hidden)
-      .map((x) => {
-        return { ...x };
-      });
+      .filter(this.isVisible)
+      .map<Task>((x) => structuredClone(x));
   }
 
   get steps() {
     // Not returning behavior subject to guard against updates from multiple sources
-    return this.step$.asObservable();
+    return this._step$.asObservable();
   }
 
   get currentStep(): Task {
@@ -31,12 +31,12 @@ export class TaskService {
     );
     // Return copy to follow principle of immutability
     // Reference: https://indepth.dev/posts/1381/immutability-importance-in-angular-applications
-    return currentStep ? { ...currentStep } : null;
+    return currentStep ?? null;
   }
 
   constructor() {
     this.initialize();
-    this.step$ = new BehaviorSubject(this._steps);
+    this._step$ = new BehaviorSubject(this.visibleSteps);
   }
 
   activateTask(taskType: TaskType) {
@@ -77,7 +77,7 @@ export class TaskService {
 
   reset() {
     this.initialize();
-    this.step$.next(this.visibleSteps);
+    this._step$.next(this.visibleSteps);
   }
 
   lockSteps(lastLockable: LastLockableTask) {
@@ -122,9 +122,12 @@ export class TaskService {
   }
 
   private patchState(steps: Tasks) {
-    this._steps = steps;
+    this._steps = steps.map((v: Task, i: number) => ({
+      ...this._steps[i],
+      ...v,
+    }));
     // dependencies don't need to know about steps that aren't available.
-    this.step$.next(this.visibleSteps);
+    this._step$.next(this.visibleSteps);
   }
 
   private initialize() {
