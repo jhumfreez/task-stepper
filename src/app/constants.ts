3c9f76e1f3b55a234dbcf4c0599b7411806f92be
jhumfreez <25644264+jhumfreez@users.noreply.github.com>
2023-02-05 13:48:54 -0600

    1 file modified

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -6,7 +6,7 @@ export const INIT_TASKS: Task[] = [
   new Task('Profile', TaskType.Profile, true, false),
   new Task('Trade-in', TaskType.TradeIn, true, true),
   new Task('Accessories', TaskType.Accessories, true, true),
-  new Task('F&I Products', TaskType.FIProducts, false, true),
+  new Task('Insurance', TaskType.FIProducts, false, true),
   new Task('Credit Application', TaskType.CreditApp, false, true),
   new Task('Documents', TaskType.DocumentUpload, true, true),
   new Task('Schedule Delivery', TaskType.ScheduleDelivery, true, false),
