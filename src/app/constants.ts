3acf30660147cf2335d41c8668a972dd41016a16
jhumfreez <25644264+jhumfreez@users.noreply.github.com>
2023-04-12 15:53:54 -0500

    1 file modified

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -6,7 +6,7 @@ export const INIT_TASKS: Task[] = [
   new Task('Profile', TaskType.Profile, true, false),
   new Task('Trade-in', TaskType.TradeIn, true, true),
   new Task('Accessories', TaskType.Accessories, true, true),
-  new Task('Insurance', TaskType.FIProducts, false, true),
+  new Task('Insurance', TaskType.Products, false, true),
   new Task('Credit Application', TaskType.CreditApp, false, true),
   new Task('Documents', TaskType.DocumentUpload, true, true),
   new Task('Schedule Delivery', TaskType.ScheduleDelivery, true, false),
@@ -18,7 +18,7 @@ export const TASK_CONFIG = new Map<TaskType, TaskConfig>([
   [TaskType.Profile, extractConfig(INIT_TASKS[TaskType.Profile])],
   [TaskType.TradeIn, extractConfig(INIT_TASKS[TaskType.TradeIn])],
   [TaskType.Accessories, extractConfig(INIT_TASKS[TaskType.Accessories])],
-  [TaskType.FIProducts, extractConfig(INIT_TASKS[TaskType.FIProducts])],
+  [TaskType.Products, extractConfig(INIT_TASKS[TaskType.Products])],
   [TaskType.CreditApp, extractConfig(INIT_TASKS[TaskType.CreditApp])],
   [TaskType.DocumentUpload, extractConfig(INIT_TASKS[TaskType.DocumentUpload])],
   [
