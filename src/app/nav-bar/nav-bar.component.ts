4db3c39ab33f6f533e8aa9ede819a1a2160606c4
jhumfreez <25644264+jhumfreez@users.noreply.github.com>
2023-05-11 23:00:54 -0500

    1 file modified

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { TaskType } from '../types';
 
@@ -8,7 +8,21 @@ import { TaskType } from '../types';
   styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent {
-  @Input() steps: Task[] = [];
+  // @Input() steps: Task[] = [];
+  @Input() nextStep: Task;
+  @Input() prevStep: Task;
+
+  // Navigate to next available step
+  @Output('next') navNext: EventEmitter<Task>;
+  // Navigate to previous available step
+  @Output('prev') navPrev: EventEmitter<Task>;
+  // Navigate to back to stepper page (return to the active/current step)
+  @Output('back') navBack: EventEmitter<Task>;
+  // Submit a document (for pages with forms) or conclude checkout process (final step)
+  @Output('upload') upload: EventEmitter<Task>;
+
   advance() {}
   previous() {}
+  back() {}
+  submit() {}
 }
